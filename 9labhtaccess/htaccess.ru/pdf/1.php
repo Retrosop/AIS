764915c2b7c82f57b6435b369a48ec1e528cb3b2
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-05-05 08:59:49 +1000

    lab9htacces

---
+++ 9labhtaccess/htaccess.ru/pdf/1.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+</head>
+<body>
+<?='Hello world' ?>
+
+
+<table border=2>
+<script>
+let a;
+for (i=1;i<10;i++){
+	a1='<td>'+i*i+'</td>';
+	a2='<td>'+i*i+'</td>';
+	a3='<td>'+i*i+'</td>';
+	a4='<td>'+i*i+'</td>';
+	a5='<td>'+i*i+'</td>';
+    document.write('<tr>'+a1+a2+a3+a4+a5+'<tr>');
+}
+</script>
+</table>
+
+</body>
+
+</html>
