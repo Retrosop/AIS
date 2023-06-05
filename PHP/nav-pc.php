e12167880e7768dc7a1e122d6c1dd7287b8774aa
rgrin2016 <r.grin.444@gmail.com>
2023-06-05 15:21:52 +1000

    Create nav-pc.php

---
+++ PHP/nav-pc.php
@@ -0,0 +1,25 @@
+<nav id="nav-pc">
+		<ul class="ul-margin">
+			<li><a href="">Главная</a></li>
+			<li><a href="" id="dpopmenu-1">Сязь</a>
+				<ul>
+					<li><a href="">Подключение</a></li>
+					<li><a href="">Тарифы</a></li>
+					<li><a href="">Оборудование</a></li>
+					<li><a href="">Услуги</a></li>
+					<li><a href="">Поддержка</a></li>
+					<li><a href="">Спутниковое ТВ</a></li>
+				</ul>
+			</li>
+			<li><a href="" id="dpopmenu-2">Сервисы</a>
+				<ul>
+					<li><a href="">Развлечения</a></li>
+					<li><a href="">Киноманам</a></li>
+					<li><a href="">Меломанам</a></li>
+					<li><a href="">Геймерам</a></li>
+				</ul>
+			</li>
+			<li><a href="">Финансы</a></li>
+			<li><a href="">Интернет-магазин</a></li>
+		</ul>
+	</nav>
