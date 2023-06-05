f310b69f9e9cda65ed9ec8d8bffbc99089a6d1f9
rgrin2016 <r.grin.444@gmail.com>
2023-06-05 15:21:49 +1000

    Create index.php

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<?php include 'head.php' ?>
+	<title>Site</title>
+</head>
+
+<body>
+<?php include 'nav-pc.php' ?>
+<?php include 'portfolio-wrap.php' ?>
+<?php include 'container.php' ?>
+</body>
+
+</html>
