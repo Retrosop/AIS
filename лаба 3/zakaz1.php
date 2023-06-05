625180e6b09b007ee507abde3b5a34485d6471bc
rgrin2016 <r.grin.444@gmail.com>
2023-06-05 15:16:19 +1000

    Create zakaz1.php

---
+++ лаба 3/zakaz1.php
@@ -0,0 +1,24 @@
+<?php 
+ $pen=$_POST['pen'];
+ $pencil=$_POST['pencil'];
+ $ruler=$_POST['ruler'];
+ $pencil_case=$_POST['pencil_case'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Результат заказа</title>
+</head>
+<body>
+ <h2>Результат заказа</h2>
+ <?php
+  echo "<font color=\"red\"><b>Заказ обработан</br><br/>Вы заказали</b></font><br/><br/>";
+  echo $pen.' ручек<br/>';
+  echo $pencil.' карандашей<br/>';
+  echo $ruler.' линеек<br/>';
+  echo $pencil_case.' пеналов';
+ ?>
+</body>
+</html>
