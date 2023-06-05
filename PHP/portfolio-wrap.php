91604e68ee5653e36f0545372d864c0e27946804
rgrin2016 <r.grin.444@gmail.com>
2023-06-05 15:21:57 +1000

    Create portfolio-wrap.php

---
+++ PHP/portfolio-wrap.php
@@ -0,0 +1,56 @@
+<div class="portfolio-wrap">
+		<div class="portfolio-item">
+			<div class="portfolio-item-wrap">
+				<a href="#t1">
+					<img src="https://www.cmswire.com/-/media/925d0422a23a4a7e98ff806c3dcbdfe0.ashx?mw=0&mh=0">
+					<div class="portfolio-item-inner">
+						<div class="portfolio-heading">
+							<h3>Тарифы</h3>
+						</div>
+						<ul>
+							<li>Акционные</li>
+							<li>Стоимость</li>
+							<li>Условия</li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<div class="portfolio-item">
+			<div class="portfolio-item-wrap">
+				<a href="#t2">
+					<img src="https://elektrik-market.ru/assets/images/catalog/etm/telekommunikaczionnoe-oborudovanie-i-sks.jpg">
+					<div class="portfolio-item-inner">
+						<div class="portfolio-heading">
+							<h3>Оборудование</h3>
+						</div>
+						<ul>
+							<li>Маршрутизаторы</li>
+							<li>Сутниковые тарелки</li>
+							<li>Патч-корд</li>
+							<li>Роутеры</li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<div class="portfolio-item">
+			<div class="portfolio-item-wrap">
+				<a href="#t3">
+					<img src="https://etimg.etb2bimg.com/photo/92739779.cms">
+					<div class="portfolio-item-inner">
+						<div class="portfolio-heading">
+							<h3>Сервисы</h3>
+						</div>
+						<ul>
+							<li>Геймерам</li>
+							<li>Киноманам</li>
+							<li>Меломанам</li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
