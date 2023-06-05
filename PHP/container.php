a1ea47da9153ce87e7839627d90a38991e965d7f
rgrin2016 <r.grin.444@gmail.com>
2023-06-05 15:21:47 +1000

    Create container.php

---
+++ PHP/container.php
@@ -0,0 +1,217 @@
+<div class="container">
+	<img src="https://www.cmswire.com/-/media/925d0422a23a4a7e98ff806c3dcbdfe0.ashx?mw=0&mh=0" width="97%">
+	<table id="t1" class="table1">
+		<caption>Тарифы и их характеристика</caption>
+		<tr>
+			<th>№ п/п</th>
+			<th>Акционный</th>
+			<th>Абонентская плата в месяц</th>
+			<th>Количество минут</th>
+			<th>Количество гигабайт</th>
+			<th>Стоимость тарифа</th>
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>Нет</td>
+			<td>560</td>
+			<td>400</td>
+			<td>60</td>
+			<td>700</td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>Нет</td>
+			<td>400</td>
+			<td>500</td>
+			<td>40</td>
+			<td>650</td>
+		</tr>
+		<tr>
+			<td>3.</td>
+			<td>Да</td>
+			<td>700</td>
+			<td>500</td>
+			<td>100</td>
+			<td>700</td>
+		</tr>
+		<tr>
+			<td>4.</td>
+			<td>Нет</td>
+			<td>450</td>
+			<td>600</td>
+			<td>20</td>
+			<td>500</td>
+		</tr>
+		<tr>
+			<td>5.</td>
+			<td>Да</td>
+			<td>300</td>
+			<td>400</td>
+			<td>160</td>
+			<td>500</td>
+		</tr>
+		<tr>
+			<td>6.</td>
+			<td>Да</td>
+			<td>600</td>
+			<td>1000</td>
+			<td>Безлемит</td>
+			<td>1700</td>
+		</tr>
+		<tr>
+			<td>7.</td>
+			<td>Нет</td>
+			<td>1000</td>
+			<td>Безлемит</td>
+			<td>Безлемит</td>
+			<td>1700</td>
+		</tr>
+		<tr>
+			<td>8.</td>
+			<td>Да</td>
+			<td>800</td>
+			<td>400</td>
+			<td>80</td>
+			<td>560</td>
+		</tr>
+	</table>
+	<img id="d1" src="https://elektrik-market.ru/assets/images/catalog/etm/telekommunikaczionnoe-oborudovanie-i-sks.jpg" width="97%">
+	<table id="t2" class="table1">
+		<caption>Оборудование</caption>
+		<tr>
+			<th>№ п/п</th>
+			<th>Тип Оборудования</th>
+			<th>Название оборудования</th>
+			<th>Установка</th>
+			<th>Абонентская плата</th>
+			<th>Стоимость</th>
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>Маршрутизатор</td>
+			<td>D-Link 1200</td>
+			<td>400</td>
+			<td>600</td>
+			<td>700</td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>Маршрутизатор</td>
+			<td>D-link 1000</td>
+			<td>500</td>
+			<td>400</td>
+			<td>650</td>
+		</tr>
+		<tr>
+			<td>3.</td>
+			<td>Спутниковая тарелка</td>
+			<td>Satellite Dish rk900</td>
+			<td>2400</td>
+			<td>300</td>
+			<td>700</td>
+		</tr>
+		<tr>
+			<td>4.</td>
+			<td>Патч-корд</td>
+			<td>Patch-kord m20</td>
+			<td>Не требуется</td>
+			<td>-</td>
+			<td>500</td>
+		</tr>
+		<tr>
+			<td>5.</td>
+			<td>Роутер</td>
+			<td>Huawei 100m</td>
+			<td>Бесплатно</td>
+			<td>160</td>
+			<td>500</td>
+		</tr>
+		<tr>
+			<td>6.</td>
+			<td>Облачное хранилище</td>
+			<td>Western-Digital 6TB</td>
+			<td>Не требуется</td>
+			<td>-</td>
+			<td>1700</td>
+		</tr>
+		<tr>
+			<td>7.</td>
+			<td>Спутниковая тарелка</td>
+			<td>Satellite Dish rk800</td>
+			<td>2000</td>
+			<td>500</td>
+			<td>1700</td>
+		</tr>
+		<tr>
+			<td>8.</td>
+			<td>ТВ-Приставка</td>
+			<td>Hiaomi s20</td>
+			<td>Не требуется</td>
+			<td>80</td>
+			<td>560</td>
+		</tr>
+	</table>
+	<img src="https://etimg.etb2bimg.com/photo/92739779.cms" width="97%">
+	<table id="t3" class="table1">
+		<caption>Сервисы</caption>
+		<tr>
+			<th>№ п/п</th>
+			<th>Тип сервиса</th>
+			<th>Название сервиса</th>
+			<th>Абонентская плата</th>
+
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>Игровой</td>
+			<td>Nvidia</td>
+			<td>400</td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>Игровой</td>
+			<td>Gforce</td>
+			<td>500</td>
+		</tr>
+		<tr>
+			<td>3.</td>
+			<td>Музыкальный</td>
+			<td>Spotify</td>
+			<td>80</td>
+		</tr>
+		<tr>
+			<td>4.</td>
+			<td>Кино</td>
+			<td>Netflix</td>
+			<td>250</td>
+		</tr>
+		<tr>
+			<td>5.</td>
+			<td>IVI</td>
+			<td>Huawei 100m</td>
+			<td>230</td>
+		</tr>
+		<tr>
+			<td>6.</td>
+			<td>Игровой</td>
+			<td>Western</td>
+			<td>600</td>
+		</tr>
+		<tr>
+			<td>7.</td>
+			<td>Музыкальный</td>
+			<td>Yandex Music</td>
+			<td>200</td>
+		</tr>
+		<tr>
+			<td>8.</td>
+			<td>Игровой</td>
+			<td>GameRock</td>
+			<td>400</td>
+		</tr>
+	</table>
+</div>
+<div class="bubble"></div>
+<div class="bubble1"></div>
+<script src="script.js"></script>
+<script src="script1.js"></script>
