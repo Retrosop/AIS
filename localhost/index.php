835c3b02b9756f9bcc90537b627c6fb8e13971af
Secret_agent <126828655+Sikret-agent@users.noreply.github.com>
2023-06-05 20:58:14 +1000

    сайт на php

---
+++ localhost/index.php
@@ -0,0 +1,298 @@
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Кондитерская "Пудра"</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/gijgo.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid ">
+                    <div class="header_bottom_border">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+                                    <a href="index.php">
+                                     <img src="img/log0.png" alt="">  
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="index.php"><h3 style="color:white">Главная</h3></a></li>
+                                            <li><a href="jobs.php"><h3 style="color:white">Каталог</h3></a></li>                                                                                
+                                                    <li><a href="job_details.php"><h3 style="color:white">О компании </h3></a></li>
+													<li><a href="contact.php"><h3 style="color:white">Контакты</h3></a></li> 
+                                                  
+                                                </ul>
+                                            </li>
+                                           
+                                                <ul class="submenu">
+                                                  
+                                                </ul>
+                                            </li>
+                                           
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <div class="slider_area">
+        <div class="single_slider  d-flex align-items-center slider_bg_1">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-7 col-md-6">
+                        <div class="slider_text">
+                            
+                            <h3 class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Кондитерская "Пудра"</h3>
+                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".4s">Миссия нашей кондитерской: дарить людям улыбки и праздник, наслаждаясь незабываемо вкусными тортами и приятным общением с нами!</p>
+                            <div class="sldier_btn wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ilstration_img wow fadeInRight d-none d-lg-block text-right" data-wow-duration="1s" data-wow-delay=".2s">
+            <img src="img/banner/11.png" alt="">
+        </div>
+    </div>
+    <!-- slider_area_end -->
+
+  
+
+    <!-- popular_catagory_area_start  -->
+    <div class="popular_catagory_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section_title mb-40">
+                        <h3>Наши популярные десерты</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-xl-3 col-md-6">
+                    <div class="single_catagory">
+                        <a href="jobs.php"><h4>Торт Нежный</h4></a>
+                         <img src="img/t1.jpg" alt="" width=200  height=200>
+                    </div>
+                </div>
+               
+                <div class="col-lg-4 col-xl-3 col-md-6">
+                    <div class="single_catagory">
+                        <a href="jobs.php"><h4>Печенье макаронс</h4></a>
+						<img src="img/ym.jpg" alt="" width=200  height=200>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-xl-3 col-md-6">
+                    <div class="single_catagory">
+                        <a href="jobs.php"><h4>Торт суфле</h4></a>
+						<img src="img/m.jpg" alt="" width=200  height=200>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-xl-3 col-md-6">
+                    <div class="single_catagory">
+                        <a href="jobs.php"><h4>Торт с ягодами</h4></a>
+						<img src="img/t2.jpg" alt="" width=200  height=200>
+                    </div>
+                </div>
+                
+                
+                </div>
+            </div>
+        </div>
+   
+    <!-- popular_catagory_area_end  -->   
+    <!-- testimonial_area  -->
+    <div class="testimonial_area  ">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section_title text-center mb-40">
+                        <h3>Отзывы</h3>
+                    </div>
+                </div>
+                <div class="col-xl-12">
+                    <div class="testmonial_active owl-carousel">
+                        <div class="single_carousel">
+                            <div class="row">
+                                <div class="col-lg-11">
+                                    <div class="single_testmonial d-flex align-items-center">
+                                        <div class="thumb">
+                                            <img src="img/testmonial/author1.png" alt="">
+                                            
+                                        </div>
+                                        <div class="info">
+                                            <p>Делала заказ трёх наборов макаронс!
+											Понравились ОООООООЧЕНЬ!
+											они ПОТРЯСАЮЩЕ ВКУСНЫЕ! САМЫЕ ПРАВИЛЬНЫЕ ПИРОЖНЫЕ!!!!
+											СПАСИБО ОГРОМНОЕ ЗА ВАШ ПРОФЕССИОНАЛИЗМ И ЛЮБОВЬ К СВОЕЙ ВКУСНОЙ ТВОРЧЕСКОЙ РАБОТЕ!!!!!</p>
+                                            <span>- Александра Маусс</span>
+                                        </div>
+										
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        </div>
+                    </div>
+					<div class="col-xl-12">
+                    <div class="testmonial_active owl-carousel">
+                        <div class="single_carousel">
+                            <div class="row">
+                                <div class="col-lg-11">
+                                    <div class="single_testmonial d-flex align-items-center">
+                                        <div class="thumb">
+                                            <img src="img/testmonial/author2.png" alt="">
+                                            
+                                        </div>
+										
+                                        <div class="info">
+                                            <p>Баланс вкуса и эстетическое наслаждение, так можно охарактеризовать фисташковый торт с сыром маскарпоне и свежей малиной. 
+											Очень вкусно, не приторно, малинка придает пикантности! Никто не остался равнодушным! 
+											Жалею что не сфотографировала торт на срезе. 
+											Рекомендую этот торт! Спасибо!</p>
+                                            <span>- Олег Иванович</span>
+                                        </div>
+										
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        </div>
+                    </div>
+					
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /testimonial_area  -->
+
+
+    <!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-md-6 col-lg-3">
+                        <div class="footer_widget wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                            <div class="footer_logo">
+                                <a href="#">
+                                 <img src="img/log0.png" alt="">     
+                                </a>
+                            </div>
+                            <p>
+                                СonditerPudra.com <br>
+                                +8 800-555-35-35 <br>
+                                ул. Советская 54а, г. Владивосток
+                            </p>
+                            <div class="socail_links">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-google-plus"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    
+    <!-- link that opens popup -->
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/gijgo.min.js"></script>
+
+
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
