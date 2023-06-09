6347390bff5602b44a62ff120455b55286512b23
YanaEkim <ekimovaayana@gmail.com>
2023-06-09 10:13:28 +1000

    itogbootstrap

---
+++ testik/jobs.php
@@ -0,0 +1,314 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Каталог</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/gijgo.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid ">
+                    <div class="header_bottom_border">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+                                    <a href="index.php">
+                                       <img src="img/log0.png" alt="">   
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="index.php"><h3 style="color:white">Главная</h3></a></li>
+                                            <li><a href="jobs.php"><h3 style="color:white">Каталог</h3></a></li>                                                                                
+                                                    <li><a href="job_details.php"><h3 style="color:white">О компании </h3></a></li>
+													<li><a href="contact.php"><h3 style="color:white">Контакты</h3></a></li> 
+                                                  
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- bradcam_area  -->
+    <div class="bradcam_area bradcam_bg_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text">
+                        <h3>Каталог</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ bradcam_area  -->
+
+    <!-- job_listing_area_start  -->
+    <div class="job_listing_area plus_padding">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-12">
+                    
+                    <div class="job_lists m-0">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <div class="single_jobs white-bg d-flex justify-content-between">
+                                    <div class="jobs_left d-flex align-items-center">
+                                        <div class="col-sm-4">
+										<img src="img/t1.jpg" alt="" width=300  height=300></div>
+                                        <div class="jobs_conetent">
+                                            <a href="job_details.php"><h4>Торт Нежный</h4></a>
+											<p style="color:black">Торт c кружевами и бабочками. С бабочками и инициалами жениха и невесты. 
+											Фото свадебных тортов с бабочками.</p>
+                                            <ul class="kitt-list kitt-list_ul">
+									<h5>Ингридиенты:</h5>
+										<li class="kitt-list-item kitt-list-item_ul">Крем чиз</li>
+										<li class="kitt-list-item kitt-list-item_ul">Джем на выбор</li>
+										<li class="kitt-list-item kitt-list-item_ul">Бисквитные коржи</li>
+									</ul>
+											
+                                        </div>
+                                    </div>
+                                    <div class="jobs_right">
+                                        <div class="apply_now">
+                                            <a class="heart_mark" href="#"> <i class="fa fa-heart"></i> </a>
+                                           
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12">
+                                <div class="single_jobs white-bg d-flex justify-content-between">
+                                    <div class="jobs_left d-flex align-items-center">
+                                        <div class="col-sm-4">
+										<img src="img/ym.jpg" alt="" width=300  height=300></div>
+                                        <div class="jobs_conetent">
+                                            <a href="job_details.php"><h4>Печенье макаронс</h4></a>
+											<p style="color:black">Макаро́н — французское кондитерское изделие из яичных белков, сахара и молотого миндаля. 
+											Обычно делается в форме печенья; между двумя слоями кладут крем или варенье.</p>
+                                            <ul class="kitt-list kitt-list_ul">
+									<h5>Ингридиенты:</h5>
+										<li class="kitt-list-item kitt-list-item_ul">Миндальное печенье</li>
+										<li class="kitt-list-item kitt-list-item_ul">Начинка на выбор</li>
+										<li class="kitt-list-item kitt-list-item_ul">глазурь для украшения</li>
+                                        </div>
+                                    </div>
+                                    <div class="jobs_right">
+                                        <div class="apply_now">
+                                            <a class="heart_mark" href="#"> <i class="fa fa-heart"></i> </a>
+                                           
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12">
+                                <div class="single_jobs white-bg d-flex justify-content-between">
+                                    <div class="jobs_left d-flex align-items-center">
+                                        <div class="col-sm-4">
+										<img src="img/m.jpg" alt="" width=300  height=300></div>
+                                        <div class="jobs_conetent">
+                                            <a href="job_details.php"><h4>Торт суфле</h4></a>
+											<p style="color:black">Торт суфле – это нежнейшее и лёгкое десертное блюдо, которое не всегда требует выпечки. 
+  Его готовят чаще всего из творога, взбитых густых сливок либо творожной массы с добавлением самых различных добавок – от кокосовой стружки, 
+  дроблёных орешков до фруктов, ягод и шоколадной начинки.</p>
+                                            <ul class="kitt-list kitt-list_ul">
+									<h5>Ингридиенты:</h5>
+										<li class="kitt-list-item kitt-list-item_ul">ванильное суфле</li>
+										<li class="kitt-list-item kitt-list-item_ul">какао</li>
+										<li class="kitt-list-item kitt-list-item_ul">шоколадный бисквит</li>
+                                        </div>
+                                    </div>
+                                    <div class="jobs_right">
+                                        <div class="apply_now">
+                                            <a class="heart_mark" href="#"> <i class="fa fa-heart"></i> </a>
+                                           
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12">
+                                <div class="single_jobs white-bg d-flex justify-content-between">
+                                    <div class="jobs_left d-flex align-items-center">
+                                        <div class="col-sm-4">
+										<img src="img/t2.jpg" alt="" width=300  height=300></div>
+                                        <div class="jobs_conetent">
+                                            <a href="job_details.php"><h4>Торт с ягодами</h4></a>
+											<p style="color:black">Ягодный торт — это кондитерское изделие, 
+											в котором обязательно присутствуют ягоды, как лесные, так и садовые.</p>
+                                            <ul class="kitt-list kitt-list_ul">
+									<h5>Ингридиенты:</h5>
+										<li class="kitt-list-item kitt-list-item_ul">свежие ягоды</li>
+										<li class="kitt-list-item kitt-list-item_ul">ягодный джем</li>
+										<li class="kitt-list-item kitt-list-item_ul">крем чиз</li>
+										<li class="kitt-list-item kitt-list-item_ul">шоколадный бисквит</li>
+                                        </div>
+                                    </div>
+                                    <div class="jobs_right">
+                                        <div class="apply_now">
+                                            <a class="heart_mark" href="#"> <i class="fa fa-heart"></i> </a>
+                                           
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- job_listing_area_end  -->
+
+    <!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-md-6 col-lg-3">
+                        <div class="footer_widget wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                            <div class="footer_logo">
+                                <a href="#">
+                                  <img src="img/log0.png" alt="">    
+                                </a>
+                            </div>
+                            <p>
+                                СonditerPudra.com <br>
+                                +8 800-555-35-35 <br>
+                                ул. Советская 54а, г. Владивосток
+                            </p>
+                   <div class="socail_links">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-google-plus"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+						</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    </footer>
+    <!--/ footer end  -->
+
+    <!-- link that opens popup -->
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <!-- <script src="js/gijgo.min.js"></script> -->
+    <script src="js/range.js"></script>
+
+
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+
+    <script src="js/main.js"></script>
+
+
+	<script>
+        $( function() {
+            $( "#slider-range" ).slider({
+                range: true,
+                min: 0,
+                max: 24600,
+                values: [ 750, 24600 ],
+                slide: function( event, ui ) {
+                    $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] +"/ Year" );
+                }
+            });
+            $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+                " - $" + $( "#slider-range" ).slider( "values", 1 ) + "/ Year");
+        } );
+        </script>
+</body>
+
+</html>
