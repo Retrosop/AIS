a17e7e98a702713cfbe749ae21f3552b1be07005
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-09 13:28:22 +1000

    Lecture - XML load

---
+++ v.glagolev/10labxml/clientxml/ClientXML/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientXML
+{
+	internal class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public string Company { get; set; }
+        public Person( string name, int age, string company)
+        {
+				Name = name;
+				Age = age;
+				Company = company;
+	    }
+
+    }
+
+
+}
---
+++ v.glagolev/10labxml/clientxml/ClientXML/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace ClientXML
+{
+	class MyProgram
+	{
+		static void Main()
+		{
+			Person tom = new Person("Tom", 25, "Micsoft");
+			List<Person> list = new List<Person>();
+			list.Add(new Person("Ivan", 34, "Google"));
+
+			XmlDocument xDoc = new XmlDocument();
+			xDoc.Load("Person.xml");
+			XmlElement? xRoot = xDoc.DocumentElement;
+			if (xRoot != null )
+			{
+				foreach (XmlElement xnode in xRoot)
+				{
+					XmlNode? attr = xnode.Attributes.GetNamedItem("name");
+                    Console.WriteLine(attr?.Value);
+                }
+			}
+
+
+
+		}
+	}
+}
